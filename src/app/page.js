7a6651be3458c39d84f4c73843956f38c66746e0
devjoseph1992 <devjosephflorina@gmail.com>
2023-11-10 22:07:45 +0800

    fixing

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,6 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
-import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
-import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
